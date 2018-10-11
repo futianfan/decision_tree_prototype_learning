b82fa1df8c67983e2bef688b8829d8261fba0b7f
futianfan <futianfan@gmail.com>
2018-10-11 11:47:00 -0400

    data preparation

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+
+TEST_FILE=./data/test_data_1.txt
+cp ./data/training_data_1.txt ./data/train_data
+INPUT_FILE=./data/train_data
+sed '1d' $TEST_FILE | awk -F "\t" '{print $3}' > ./data/test_feature
+sed '1d' $TEST_FILE | awk '{print $1}' | sed 's/True/1/;s/False/0/' > ./data/test_label
+
+
+
+### for iteration 
+
+sed '1d' $INPUT_FILE | awk '{print $1}' | sed 's/True/1/;s/False/0/' > ./data/train_label
+sed '1d' $INPUT_FILE | awk -F "\t" '{print $3}' > ./data/train_feature
+n=`python src/findmax_N.py ./data/train_feature`
+((n=n+1))
+echo $n 
+
+
 
 
 
